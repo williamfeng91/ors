21ca78332f28e78a473c13be4e522df278ee9808
William Feng <williamf@webrecs.com.au>
2015-10-11 02:03:22 +1100

    Added Autocheck BPEL

--- PDVCheck/src/main/java-generated/au/edu/unsw/soacourse/pdv/package-info.java
+++ PDVCheck/src/main/java-generated/au/edu/unsw/soacourse/pdv/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://pdv.soacourse.unsw.edu.au")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://pdv.soacourse.unsw.edu.au", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package au.edu.unsw.soacourse.pdv;
